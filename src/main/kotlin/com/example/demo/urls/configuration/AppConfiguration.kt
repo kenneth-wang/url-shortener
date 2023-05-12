a055cc8878199c50b7af07cefc4105edf8d8c314
kenneth-wangg <kenneth@plannerbee.co>
2023-05-12 17:12:00 +0800

    Move baseBackendUrl environment variable out of Spring context

--- src/main/kotlin/com/example/demo/urls/configuration/AppConfiguration.kt
+++ src/main/kotlin/com/example/demo/urls/configuration/AppConfiguration.kt
@@ -8,6 +8,6 @@ import org.springframework.context.annotation.PropertySource
 @PropertySource("classpath:application.yml")
 class AppConfiguration {
 
-    @Value("\${spring.baseBackendUrl}")
+    @Value("\${baseBackendUrl}")
     lateinit var baseBackendUrl: String
 }
