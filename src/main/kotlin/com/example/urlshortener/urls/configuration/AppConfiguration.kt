aaf6c7e59de3d2cc7253e9c16e3866b6cf18bb12
kenneth-wangg <kenneth@plannerbee.co>
2023-05-14 20:58:46 +0800

    Split application.yml into two files, one for each spring profile

--- src/main/kotlin/com/example/urlshortener/urls/configuration/AppConfiguration.kt
+++ src/main/kotlin/com/example/urlshortener/urls/configuration/AppConfiguration.kt
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.PropertySource
 
 @Configuration
-@PropertySource("classpath:application.yml")
 class AppConfiguration {
 
     @Value("\${baseBackendUrl}")
